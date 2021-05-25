ad5404ee70206cf76417ba3273b0ec80f9d7847e
devAdhiraj <75645547+devAdhiraj@users.noreply.github.com>
2021-05-25 10:17:57 -0400

    Added more functions and comments

--- JugPuzzle.py
+++ JugPuzzle.py
@@ -4,40 +4,46 @@ class Jug:
         self.water = water
         self.capacity = capacity
 
-    
+    def __str__(self):
+        return f"({self.water}/{self.capacity})"
+
+
 class Puzzle:
-    high_score = 0
+    high_score = 999999
     low_score = 0
 
     def __init__(self):
         self.moves = 0
         self.jugs = [Jug(8, 8), Jug(0, 5), Jug(0, 3)]
     
+    
+    def __str__(self):
+        return f"{self.moves}\t0:{self.jugs[0]}\t1:{self.jugs[1]}\t2:{self.jugs[2]}"
 
+    # Main function where game is played
     def play(self):
-        while(self.jugs[0].water != 4 or self.jugs[1].water != 4):
-            print(f"{self.moves} {self.print_helper(0)} {self.print_helper(1)} {self.print_helper(2)}")
-
-            jugFrom = int(input("Spill from jug: "))
-            jugTo = int(input("into jug: "))
-            self.transfer_water(jugFrom, jugTo)
-
+        while not self.is_game_won():
+            print(self)
+            self.get_move()
             self.moves += 1
         
-        print(f"\nConrats you won in {self.moves} moves!!!")
-        
+        print(f"\nCongrats you won in {self.moves} moves!!!")
         self.set_score()
         self.play_again()
 
+    # Checks if game is won
+    def is_game_won(self):
+        return self.jugs[0].water == 4 and self.jugs[1].water == 4
 
-    def print_helper(self, jugNumber):
-        return f"{jugNumber}: ({self.jugs[jugNumber].water}/{self.jugs[jugNumber].capacity})"
+    # Takes input and calls transfer water
+    def get_move(self):
+        jugFrom = int(input("Spill from jug: "))
+        jugTo = int(input("into jug: "))
 
+        self.transfer_water(self.jugs[jugFrom], self.jugs[jugTo])
 
+    # Transfers water between jugs
     def transfer_water(self, jugFrom, jugTo):
-        jugFrom = self.jugs[jugFrom]
-        jugTo = self.jugs[jugTo]
-
         if jugFrom.water > jugTo.capacity - jugTo.water:
             jugFrom.water -= (jugTo.capacity - jugTo.water)
             jugTo.water = jugTo.capacity
@@ -45,19 +51,19 @@ class Puzzle:
             jugTo.water += jugFrom.water
             jugFrom.water = 0
 
-    
+    # Asks to play again, resets instance if yes and starts new game
     def play_again(self):
-        ask = input("\nDo you want to play again?\ny/n - ")
-        if(ask == "y"):
+        if(input("\nDo you want to play again?\ny/n - ") == "y"):
             print()
             self.__init__()
             self.play()
 
-
+    # sets the high score and low score
     def set_score(self):
         if self.moves > Puzzle.low_score:
             Puzzle.low_score = self.moves
-        if Puzzle.high_score == 0 or Puzzle.high_score > self.moves:
+
+        if Puzzle.high_score > self.moves:
             Puzzle.high_score = self.moves
 
         print(f"Highest score: {Puzzle.high_score} moves")
@@ -65,4 +71,3 @@ class Puzzle:
 
 
 Puzzle().play()
-
